9d0131ce6a77900cbcbd2059c6fabe674a63c4c8
Duarte R <raposoduca@gmail.com>
2022-04-22 22:11:07 +0100

    auto launch scripts implemented

--- launch_client.sh
+++ launch_client.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-
+cd ./bftsmart/build/install/library
+./smartrun.sh bft_dti.BFT_DTIClient
--- launch_server.sh
+++ launch_server.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 
+cd ./bftsmart/build/install/library
+./smartrun.sh bft_dti.BFTMapServer $@
